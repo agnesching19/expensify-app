6c1cd44707a09e23909d0c82b580e2085200ddae
Agnes Ching <agnesching19@gmail.com>
2018-02-23 17:45:18 +0000

    modified 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
   <div>
-    404 - <Link to="/">Go to your mama</Link>
+    404 - <Link to="/">GO BACK TO DASHBOARD</Link>
   </div>
 );
 
