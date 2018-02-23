7d4de3a20d667cf0d092ca6a7c7ecbf8af2bbaa7
Agnes Ching <agnesching19@gmail.com>
2018-02-23 18:22:02 +0000

    404 image and div

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">GO BACK TO DASHBOARD</Link>
+  <div className="content-container">
+    <div className="not-found__404">
+      <img className="not-found__image" src="/images/404.png" />
+    </div>
+    <div className="not-found__link">
+      <Link to="/">GO BACK TO DASHBOARD</Link>
+    </div>
   </div>
 );
 
